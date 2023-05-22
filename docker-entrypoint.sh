e314f4b1cb7ac85c3f222f46f19ae0fdbde1f03a
alexcos20 <alex.coseru@gmail.com>
2023-05-22 10:06:29 +0300

    updates

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "${WAIT_FOR_SUBGRAPH}" = "true" ]
+then
+  while [ ! -f "/ocean-subgraph/ready" ]; do
+    echo "Waiting for subgraph to be ready...."
+    sleep 2
+  done
+fi
+cd /pdr-trueval/
+echo "Starting app..."
+/usr/local/bin/python main.py 2>&1 &
+tail -f /dev/null
+
--- pdr_trueval/utils/process.py
+++ pdr_trueval/utils/process.py
@@ -5,19 +5,19 @@ from pdr_trueval.utils.threads import NewTrueVal
 
 
 """ Get all intresting topics that we can submit trueval """
-topics = get_all_interesting_prediction_contracts()
+topics = []
 def process_block(block):
     global topics
     """ Process each contract and see if we need to submit """
-    print(f"Got new block: {block['number']}...")
     if not topics:
         topics = get_all_interesting_prediction_contracts()
+    print(f"Got new block: {block['number']} with {len(topics)} topics")
     threads=[]
     for address in topics:
         topic = topics[address]
         predictor_contract = PredictorContract(address)
         epoch = predictor_contract.get_current_epoch()
-        if epoch > topic['last_submited_epoch']:
+        if epoch > topic['last_submited_epoch'] and epoch>2:
             """ Let's make a prediction & claim rewards"""
             thr = NewTrueVal(topic,predictor_contract,block["number"],epoch)
             thr.start()
