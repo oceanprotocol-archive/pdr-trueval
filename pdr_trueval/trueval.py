92b6870737795c1275c59ee3d67ce606a3431b9b
alexcos20 <alex.coseru@gmail.com>
2023-07-06 20:36:41 +0300

    quick fix

--- pdr_trueval/trueval.py
+++ pdr_trueval/trueval.py
@@ -6,11 +6,9 @@ def get_true_val(topic, contract_address, initial_timestamp, end_timestamp):
     kraken = ccxt.kraken()
 
     topic = topic.replace("-", "/")
-
-    price_initial = kraken.fetch_ohlcv(topic, "1m", since=initial_timestamp, limit=1)
-    price_end = kraken.fetch_ohlcv(topic, "1m", since=end_timestamp, limit=1)
-
-    if price_end[0][1] == price_initial[0][1]:
-        return (True,price_end[0][1],True)
-
-    return (price_end[0][1] > price_initial[0][1],price_end[0][1],False)
+    try:
+        price_initial = kraken.fetch_ohlcv(topic, "1m", since=initial_timestamp, limit=1)
+        price_end = kraken.fetch_ohlcv(topic, "1m", since=end_timestamp, limit=1)
+        return (price_end[0][1] >= price_initial[0][1],price_end[0][1],False)
+    except Exception as e:
+        return(False,0,True)
